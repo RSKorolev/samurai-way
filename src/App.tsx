5810ae0b7ace771453c66c8df341fe1b786afee7
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-03 21:22:47 +0700

    import components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./Header";
+import Technologies from "./Technologies";
 function App() {
   return (
     <div className="App">
