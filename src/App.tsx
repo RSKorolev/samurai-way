0f888c590065d2518669af99e1b490bb8eaa0994
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-06 21:32:03 +0700

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,14 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img
-          src="https://i1.wp.com/d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4866092.jpg"
-          alt="logo"
-        />
-      </header>
-      <nav className="nav">
-        <div>
-          <a> Profile </a>
-        </div>
-        <div>
-          <a>Message</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <img
-          src="https://www.desktopbackground.org/download/800x600/2014/11/13/855442_800x600-wallpapers-images-tbwnz-com_800x600_h.jpg"
-          alt="idea"
-        />
-        <div>avatar+description</div>
-        <div>
-          My post
-          <div>New post</div>
-        </div>
-        <div>
-          <div>Post 1</div>
-          <div>Post 2</div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://i1.wp.com/d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4866092.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+export default Header;
